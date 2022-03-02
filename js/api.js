a564b0d42f7cc59ac8bd3f4172a326ea934fa2eb
nishattasnim08 <nishattasnim24258@gmail.com>
2022-03-02 15:51:18 +0600

    phone brand commited

--- js/api.js
+++ js/api.js
@@ -23,10 +23,10 @@ const displaySearchResult = phones => {
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-            <img src="${phone.image}" class="w-50 h-75 my-3 mx-auto card-img-top" alt="...">
+            <img src="${phone.image}" class="w-50 h-75 my-5 mx-auto card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="text-center card-title">${phone.phone_name}</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text text-center">${phone.brand}</p>
                 </div>
         </div>
         `;
