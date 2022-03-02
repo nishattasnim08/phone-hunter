575e6a64fb1b61e8fe35d0561c3f9005a3c14925
nishattasnim08 <nishattasnim24258@gmail.com>
2022-03-02 14:41:44 +0600

    display card commited

--- js/api.js
+++ js/api.js
@@ -1,8 +1,12 @@
 const searchPhone = () =>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    
+    searchField.value = '';
+
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
 
-    console.log(url);
+    
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data));
 }
