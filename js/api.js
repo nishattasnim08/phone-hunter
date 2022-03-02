6d5ab4c1a2a331993ddeb6b855976c80b3cd5f9a
nishattasnim08 <nishattasnim24258@gmail.com>
2022-03-02 17:58:57 +0600

    phone details commited

--- js/api.js
+++ js/api.js
@@ -14,23 +14,61 @@ const searchPhone = () => {
 // search result display
 
 const displaySearchResult = phones => {
-    const searchResult = document.getElementById('search-result');
-    phones.forEach(phone => {
-        console.log(phone);
 
-        // dynamic display card creating
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
+    if (phones.length == 0) {
+        alert("No Phone Found");
+    }
+    else {
+        const searchResult = document.getElementById('search-result');
+        phones.forEach(phone => {
+            // console.log(phone);
+
+            // dynamic display card creating
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
         <div class="card h-100">
             <img src="${phone.image}" class="w-50 h-75 my-5 mx-auto card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="text-center card-title">${phone.phone_name}</h5>
                     <p class="card-text text-center">${phone.brand}</p>
+                    <button type="button" onclick="phoneDetail('${phone.slug}')" class="btn btn-primary">Primary</button>
                 </div>
         </div>
         `;
 
-        searchResult.appendChild(div);
-    });
+            searchResult.appendChild(div);
+        });
+    }
+}
+
+// phone details
+const phoneDetail = phoneId => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetail(data.data));
+}
+
+// display details
+const displayPhoneDetail = phone => {
+    const phoneDetails = document.getElementById('phone-detail');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <div class="card-body">
+    <img src="${phone.image}" class="w-50 h-75 my-5 mx-auto card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${phone.name}</h5>
+      <p class="card-text">Storage: ${phone.mainFeatures.storage}</p>
+      <p class="card-text">Display Size: ${phone.mainFeatures.displaySize}</p>
+      <p class="card-text">Chipset: ${phone.mainFeatures.chipSet}</p>
+      <p class="card-text">Memory: ${phone.mainFeatures.memory}</p>
+      <p class="card-text">Sensors: ${phone.mainFeatures.sensors.join(' ,')}</p>
+    </div>
+    `;
+    phoneDetails.appendChild(div);
+
+
 }
