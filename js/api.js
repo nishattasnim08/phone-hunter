3c48df667ad067bc7c29e99eedb88c2c8fc6bce2
nishattasnim08 <nishattasnim24258@gmail.com>
2022-03-02 15:40:19 +0600

    phone image, phone name and footer commited

--- js/api.js
+++ js/api.js
@@ -1,12 +1,36 @@
-const searchPhone = () =>{
+const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = '';
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
 
-    
+
     fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data));
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.data));
+}
+
+// search result display
+
+const displaySearchResult = phones => {
+    const searchResult = document.getElementById('search-result');
+    phones.forEach(phone => {
+        console.log(phone);
+
+        // dynamic display card creating
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="${phone.image}" class="w-50 h-75 my-3 mx-auto card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="text-center card-title">${phone.phone_name}</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+        </div>
+        `;
+
+        searchResult.appendChild(div);
+    });
 }
