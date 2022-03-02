c6d51a06bdfe63b8c16369a141afa8a57246ce7a
nishattasnim08 <nishattasnim24258@gmail.com>
2022-03-02 19:04:09 +0600

    final commitement

--- js/api.js
+++ js/api.js
@@ -14,7 +14,8 @@ const searchPhone = () => {
 // search result display
 
 const displaySearchResult = phones => {
-
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
     if (phones.length == 0) {
         alert("No Phone Found");
     }
@@ -29,10 +30,10 @@ const displaySearchResult = phones => {
             div.innerHTML = `
         <div class="card h-100">
             <img src="${phone.image}" class="w-50 h-75 my-5 mx-auto card-img-top" alt="...">
-                <div class="card-body">
+                <div class="card-body text-center">
                     <h5 class="text-center card-title">${phone.phone_name}</h5>
                     <p class="card-text text-center">${phone.brand}</p>
-                    <button type="button" onclick="phoneDetail('${phone.slug}')" class="btn btn-primary">Primary</button>
+                    <button type="button" onclick="phoneDetail('${phone.slug}')" class="btn btn-primary">Details >></button>
                 </div>
         </div>
         `;
@@ -54,10 +55,11 @@ const phoneDetail = phoneId => {
 // display details
 const displayPhoneDetail = phone => {
     const phoneDetails = document.getElementById('phone-detail');
+    phoneDetails.textContent = '';
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
-    <div class="card-body">
+    <div class="card-body text-center mx-auto w-50">
     <img src="${phone.image}" class="w-25 h-50 my-5 mx-auto card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${phone.name}</h5>
@@ -65,10 +67,30 @@ const displayPhoneDetail = phone => {
       <p class="card-text">Display Size: ${phone.mainFeatures.displaySize}</p>
       <p class="card-text">Chipset: ${phone.mainFeatures.chipSet}</p>
       <p class="card-text">Memory: ${phone.mainFeatures.memory}</p>
-      <p class="card-text">Sensors: ${phone.mainFeatures.sensors.join(' ,')}</p>
+      <p class="card-text">Sensors: ${phone.mainFeatures.sensors.join(', ')}</p>
       <p class="card-text">Release Date: ${phone?.releaseDate ? phone.releaseDate : "Release date has not declared"}</p>
+      <h5>Other Features :</h5>
     </div>
     `;
+
+    const otherInfo = document.getElementById('others');
+    if (!phone.others) {
+        otherFeature = document.createElement('div');
+            otherFeature.innerHTML = `
+            <p class="text-center">No More Features!</p>
+            `;
+            div.appendChild(otherFeature);
+    }
+    else{
+        for(key in phone.others){
+            console.log(key);
+            otherFeature = document.createElement('div');
+            otherFeature.innerHTML = `
+            <p class="text-center">${key}: ${phone.others[key]}</p>
+            `;
+            div.appendChild(otherFeature);
+        }
+    }
     phoneDetails.appendChild(div);
 
 
